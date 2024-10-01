313683d921f644ecdf62a230690d8fcaf544638c
jgrissom <jgrissom91@gmail.com>
2024-09-30 20:52:35 -0500

    [1.09] Data Model from Controller to View

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,5 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ProductController : Controller
 {
-  public IActionResult Index() => View();
+  public List<Product> Products = new List<Product> {
+    new Product(){ Id = 1, Name="Fishing Pole", Price = 1.99 },
+    new Product(){ Id = 2, Name="Kite String", Price = 2.99 },
+    new Product(){ Id = 3, Name="Silly Putty", Price = 3.99 }
+  };
+  public IActionResult Index() => View(Products);
 }
